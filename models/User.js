5f9bfc908ae0d0847bb49e5e64d4b62b34921d58
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-05-14 14:08:05 +0200

    Blir server-error när jag försöker registera användare... Vet inte varför

---
+++ models/User.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// User schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 10
+    }
+});
+
+// Hash password
+userSchema.pre('save', async function(next) {
+    try {
+        if(this.isNew || this.isModified('password')) {
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashedPassword;
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Register user
+userSchema.statics.register = async(username, password) => {
+    try {
+        const user = new this({ username, password });
+        await user.save();
+        return user;
+    } catch (error) {
+        throw error;
+    }
+} 
+
+// Login user
+userSchema.statics.login = async(username, password) => {
+    try {
+        const user = await this.findOne({ username });
+
+        if(!user) {
+            throw new Error('Incorrect username/password');
+        }
+        // Compare passwords
+        const isPasswordMatched = await user.comparePassword(password);
+
+        // Incorrect?
+        if(!isPasswordMatched) {
+            throw new Error('Incorrect username/password');
+        }
+        // Correct
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Compare hashed password
+userSchema.methods.comparePassword = async(password) => {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw error;
+    }
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 require('dotenv').config();
 
+// Connect to MongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error when trying to connect to database..');
+});
 
 
 router.post('/register', async(req, res) => {
@@ -22,7 +30,9 @@ router.post('/register', async(req, res) => {
             return res.status(400).json({ error });
         }
         // Succeded call
-        res.status(200).json({ message: 'User created' });
+        const user = new User({ username, password });
+        await user.save();
+        res.status(201).json({ message: 'User created' });
     } catch (error) {
         res.status(500).json({ error: 'Server error' });
     }
